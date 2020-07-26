bec9376e4e17ecdfae2114bff61274b5b207def8
10susumu <s.i.19921029@gmail.com>
2020-07-26 10:49:01 +0900

    既読機能フロント実装
    既読機能のフロント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
-    redirect_to action: ;index
+    redirect_to action: :index
   end
 
   def checked
@@ -20,6 +20,6 @@ class PostsController < ApplicationController
     end
 
     item = Post.find(params[:id])
-    render json: { post: item }
+    render json:{ post: item }
   end
 end
